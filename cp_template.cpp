6c15eb93659f80a3f7e714e85d9e39e10d06ba5b
Gagandeep Singh <123713630+gaganokay@users.noreply.github.com>
2024-05-31 10:02:43 +0530

    Update cp_template.cpp

--- cp_template.cpp
+++ cp_template.cpp
@@ -5,19 +5,25 @@ using namespace std;
 
 #define endl '\n'
 #define int long long
+#define float double
 #define sqrt sqrtl
+#define inputVector(v, n) for(int i = 0; i < n; i++) cin >> v[i]
+#define input2dArray(arr, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) cin >> arr[i][j]
+#define printVector(v, n) for(int i = 0; i < n; i++) cout << v[i] << " "; cout << endl
+#define sort(arr) sort(arr.begin(), arr.end())
+#define sortReverse(arr) sort(arr.rbegin(), arr.rend())
+const int mod = 1e9 + 7;
+const int inf = LLONG_MAX >> 1;
 
-const int MOD = 1e9 + 7;
-const int INF = LLONG_MAX >> 1;
+void solve() {
+}
 
 signed main() {
-    ios::sync_with_stdio(false); cin.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int t = 1;
     // cin >> t;
-
-    while(t--) {
-    }
+    while(t--) solve();
 
     return 0;
 }
